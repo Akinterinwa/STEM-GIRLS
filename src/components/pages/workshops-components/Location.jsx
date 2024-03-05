494ede0800a9187331933b624f446519b2906340
Akinterinwa Oluwanifemi <118985490+Akinterinwa@users.noreply.github.com>
2024-03-05 16:40:34 +0100

    added new components and adjusted the screen sizes accordingly

--- src/components/pages/workshops-components/Location.jsx
+++ src/components/pages/workshops-components/Location.jsx
@@ -7,6 +7,18 @@ const Location = () => {
         <div className="location-hero">
             <h1>Check Our Current Location</h1>
         </div>
+        <div className="location">
+          <h2>STEM Like a Girl currently offers workshops in the following locations:</h2>
+
+          <div className="map">
+          <iframe width="100%" height="600" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.com/maps?width=100%25&amp;height=600&amp;hl=en&amp;q=1%20Grafton%20Street,%20Dublin,%20Ireland+(Inspire%20Stem%20Girls)&amp;t=&amp;z=14&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"><a href="https://www.gps.ie/">gps trackers</a></iframe>
+          </div>
+          <div className="cta">
+            <p className='cta-link'>Click here for more information on our workshops in these areas</p>
+            <p className='cta-link'>Click here to get involved as a volunteer in one of these areas</p>
+            <p className='cta-link'>Click here for information on starting a STEM Like a Girl site in a new location</p>
+          </div>
+        </div>
     </>
   )
 }
--- src/components/pages/workshops-components/StartNew.jsx
+++ src/components/pages/workshops-components/StartNew.jsx
@@ -1,11 +1,46 @@
 import React from 'react';
 import '../workshops-components/Workshop.css';
+import SiteImg from '../../../images/image1.jpg';
 
 const StartNew = () => {
   return (
     <>
         <div className="startnew-hero">
             <h1>Start A New Location</h1>
+        </div>
+        <div className="new-site">
+          <div className="site-container">
+            <div className="site-image">
+              <img src={SiteImg} alt="site" />
+            </div>
+            <div className="site-content">
+              <p>Ready to excite and empower girls with knowledge and confidence in STEM? Explore this page to learn how you can bring STEM Like a Girl to your city to encourage girls to become future problem solvers and leaders.</p>
+              <div className="btn-1 button">Apply to Start a Site</div>
+              <div className="btn-1 button">Site Recruitment Package</div>
+            </div>
+          </div>
+          <div className="responsibility">
+            <h1>Site Leader Responsibilities</h1>
+            <ul>
+              <li>Inspire a group of girls in your community with the awesome potential of STEM.</li>
+              <li>Act as the main contact for STEM Like a Girl participants, volunteers, and donors in your city and STEM Like a Girl chapter planning before and throughout the program.</li>
+              <li>Act as the main contact for STEM Like a Girl participants, volunteers, and donors in your city and STEM Like a Girl chapter planning before and throughout the program.</li>
+              <li>Act as the main contact for STEM Like a Girl participants, volunteers, and donors in your city and STEM Like a Girl chapter planning before and throughout the program.</li>
+              <li>Act as the main contact for STEM Like a Girl participants, volunteers, and donors in your city and STEM Like a Girl chapter planning before and throughout the program.</li>
+              <li>Act as the main contact for STEM Like a Girl participants, volunteers, and donors in your city and STEM Like a Girl chapter planning before and throughout the program.</li>
+              <li>Act as the main contact for STEM Like a Girl participants, volunteers, and donors in your city and STEM Like a Girl chapter planning before and throughout the program.</li>
+            </ul>
+            
+            <div className="double-image">
+                <div className="image-1">
+                  <img src={SiteImg} alt="site" />
+                </div>
+                <div className="image-1">
+                  <img src={SiteImg} alt="site" />
+                </div>
+            </div>
+            <div className="btn-1 button">Apply To Start A Site</div>
+          </div>
         </div>
     </>
   )
