e9f1891e27c9f64f17c6ab7a59978d6fccde96b4
Akinterinwa Oluwanifemi <118985490+Akinterinwa@users.noreply.github.com>
2024-05-17 12:46:46 +0100

    adjusted the contact form

--- src/components/pages/connect-component/ContactUs.jsx
+++ src/components/pages/connect-component/ContactUs.jsx
@@ -11,7 +11,7 @@ const ContactUs = () => {
           <p>Email us at <span className='email'>inspirestemgirl@gmail.com</span> or complete the form below to contact Inspire STEM Girls</p>
         </div>
         <div className="contact-form">
-          <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSdM7JMhjvG7_bVV9wjyTSqFOmEBcwtL6UQwjER3SOdjRPoxrw/viewform?embedded=true" width="640" height="865" frameborder="0" marginheight="0" marginwidth="0" title='contact'>Loading…</iframe>
+        <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSfUXpMPEY2ZVz2v8P7bl4YwJYt3FmlhXmGw_AxKHWhQyWBXyg/viewform?embedded=true" width="640" height="957" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>
         </div>
         <div className="ending-p">
           <p>*At Inspire STEM Girls (ISG), we prioritize your privacy and safeguard your personal information. Rest assured, we never share your details with third parties. By sharing your name, email, and phone number, you consent to receiving our newsletter, event reminders, and educational resources. You can opt out at any time</p>
