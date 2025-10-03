9f8470b0137af82114904c7aa84bd3b5b55144ed
akinterinwaoluwanifemi <akinterinwaoluwanifemi@gmail.com>
2025-10-03 08:22:28 +0000

    fix: update nodejs version to 22

--- .idx/dev.nix
+++ .idx/dev.nix
@@ -1,18 +1,18 @@
 {pkgs}: {
   channel = "stable-24.05";
   packages = [
-    pkgs.nodejs_20
+    pkgs.nodejs
   ];
   idx.extensions = [
-    "svelte.svelte-vscode"
+    "svelte.svelte-vscode",
     "vue.volar"
   ];
   idx.previews = {
     previews = {
       web = {
         command = [
-          "npm"
-          "run"
+          "npm",
+          "run",
           "start"
         ];
         env = {
